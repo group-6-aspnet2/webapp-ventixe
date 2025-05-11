5a7b923ee09387f6c9ac56bc140938eaa67aabee
PetraElgemyr <petra.elgemyr@gmail.com>
2025-05-11 16:40:09 +0200

    add basic layouts

--- Frontend/src/partials/pages/auth/SignUp.jsx
+++ Frontend/src/partials/pages/auth/SignUp.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
-import { useAuth } from '../../../contexts/AuthContext'
+import React from "react";
+// import { useAuth } from "../../../contexts/AuthContext";
 
 const SignUp = () => {
-  try {
-    const { signUp } = useAuth()
-  }
-  catch { }
-  
+  // try {
+  //   const { signUp } = useAuth();
+  // } catch {}
 
-  return (<>
-    <div>SignUp</div>
-    <p>halloj</p></>
-  )
-}
+  return (
+    <div className="card">
+      <div className="card-header">
+        <h3>Sign Up</h3>
+      </div>
+      <div className="card-body">
+        <form>
+          <input placeholder="Email" />
+          <input placeholder="Password" />
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default SignUp
+export default SignUp;
--- Frontend/src/routing/ProtectedRoute.jsx
+++ Frontend/src/routing/ProtectedRoute.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import { Navigate } from 'react-router-dom'
-import { useAuth } from '../contexts/AuthContext'
+import React from "react";
+import { Navigate } from "react-router-dom";
+// import { useAuth } from '../contexts/AuthContext'
 
-const ProtectedRoute = ({children}) => {
-    try {
-        const { isAuthenticated } = useAuth()
-     
-        if (isAuthenticated && isAuthenticated !== undefined) {
-            return children
-        }
-    
+const ProtectedRoute = ({ children }) => {
+  try {
+    // const { isAuthenticated } = useAuth()
+    const isAuthenticated = true;
+
+    if (isAuthenticated && isAuthenticated !== undefined) {
+      return children;
     }
-    catch { }
+  } catch {
+    return <></>;
+  }
 
-    return <Navigate to="/login" replace />
-}
+  return <Navigate to="/login" replace />;
+};
 
-export default ProtectedRoute
+export default ProtectedRoute;
