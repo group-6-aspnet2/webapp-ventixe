e62dfaa6b6108ef2c06ba99dd0ead17467c7064a
PetraElgemyr <petra.elgemyr@gmail.com>
2025-05-21 11:58:37 +0200

    fix eventdate and time styling on bookingcards

--- Frontend/src/partials/BookingCard.jsx
+++ Frontend/src/partials/BookingCard.jsx
@@ -7,13 +7,12 @@ const BookingCard = ({ booking, isAdmin }) => {
     <div key={booking.id} className="booking-card">
       <div className="booking-card-header">
         <span className="card-title">{booking.eventName}</span>
-
         <StatusButton status={booking.statusName.toUpperCase()}>
           {booking.statusName}
         </StatusButton>
       </div>
-      <p>{booking.eventDate}</p>
-      <p>{booking.eventTime}</p>
+      <p className="card-event-date">{booking.eventDate}</p>
+      <p className="card-event-time">{booking.eventTime}</p>
       <div className="booking-category-container">
         <span>
           {booking.ticketQuantity}x {booking.ticketPrice}
@@ -27,6 +26,16 @@ const BookingCard = ({ booking, isAdmin }) => {
           Customer: {booking.firstName} {booking.lastName}
         </p>
       )}
+      <p>
+        Created:{" "}
+        {new Date(booking.createDate).toLocaleString("sv-SE", {
+          year: "numeric",
+          month: "2-digit",
+          day: "2-digit",
+          hour: "2-digit",
+          minute: "2-digit",
+        })}
+      </p>
     </div>
   );
 };
