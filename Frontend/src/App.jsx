67b2a5bd50efe17b4e30c0c6ccbc99cf0ac8c37e
PetraElgemyr <petra.elgemyr@gmail.com>
2025-05-21 20:55:34 +0200

    test api fetch tickets for specific userId

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,7 +4,7 @@ import RouteRenderer from "./routing/RouteRenderer";
 
 function App() {
   // note fr Petra: hårdkodad in userId i localStorage för att kunna hämta bokningar utefter userId
-  localStorage.setItem("userId", "08354a9b-217c-4974-af30-68208ba52f1b");
+  localStorage.setItem("userId", "2d4a1186-69e9-4782-b96e-7752abf3b613");
 
   return (
     <Suspense fallback={<div className="spinner">Loading...</div>}>
