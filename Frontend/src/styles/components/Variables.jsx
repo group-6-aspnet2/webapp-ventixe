894635a253f4bb751fd014835b725056b7e06b90
PetraElgemyr <petra.elgemyr@gmail.com>
2025-05-21 17:57:50 +0200

    add variables for styled components

--- Frontend/src/styles/components/Variables.jsx
+++ Frontend/src/styles/components/Variables.jsx
@@ -3,8 +3,16 @@ export const colors = {
   primary_50: "#FCD3FE",
   secondary_100: "#37437D",
   grey_10: "#FFFFFF",
+  grey_20: "#F7F7F7",
   grey_30: "#EDEDED",
   grey_80: "#636365",
   cool_grey_20: "#E4E5F5",
   cool_grey_40: "#D1D2E0",
+};
+
+export const devices = {
+  tablet: "min-width: 768px",
+  laptop: "min-width: 1024px",
+  laptopL: "min-width: 1440px",
+  fourK: "min-width: 2560px",
 };
