df0f3431d44d0b901c3681ba06f9192f8f650fd0
PetraElgemyr <petra.elgemyr@gmail.com>
2025-05-27 09:28:48 +0200

    fix console error

--- Frontend/src/partials/EventCard.jsx
+++ Frontend/src/partials/EventCard.jsx
@@ -1,42 +1,55 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-import { AdminCategoryButtonSmall, CreateBookingButton, EventCategoryButton, } from '../styles/components/StyledButton'
-import BookingModalLayout from './layouts/BookingModalLayout';
+import {
+  AdminCategoryButtonSmall,
+  CreateBookingButton,
+  EventCategoryButton,
+} from "../styles/components/StyledButton";
+import BookingModalLayout from "./layouts/BookingModalLayout";
 
 const EventCard = ({ event, getEvents }) => {
-    const [showModal, setShowModal] = useState(false);
-    const [successMessage, setSuccessMessage] = useState('');
+  const [showModal, setShowModal] = useState(false);
+  const [successMessage, setSuccessMessage] = useState("");
 
   return (
     <>
-    <div key={event.eventId} className="event-card">
+      <div key={event.eventId} className="event-card">
         <div className="event-card-header">
-            <EventCategoryButton>
-                {event.eventCategoryName}
-            </EventCategoryButton>
-            <AdminCategoryButtonSmall eventStatus={event.eventStatus}>
-                {event.eventStatus}
-            </AdminCategoryButtonSmall>
+          <EventCategoryButton>{event.eventCategoryName}</EventCategoryButton>
+          <AdminCategoryButtonSmall eventStatus={event.eventStatus}>
+            {event.eventStatus}
+          </AdminCategoryButtonSmall>
         </div>
 
         <div className="event-card-body">
-            <p>{event.eventDate} {event.eventTime}</p>
-            <h3 className="event-card-title">{event.eventName}</h3>
-            <p><i className="fa-light fa-location-dot"></i>{event.eventLocation}</p>
+          <p>
+            {event.eventDate} {event.eventTime}
+          </p>
+          <h3 className="event-card-title">{event.eventName}</h3>
+          <p>
+            <i className="fa-light fa-location-dot"></i>
+            {event.eventLocation}
+          </p>
         </div>
 
         <div className="event-card-footer">
-            <CreateBookingButton onClick={() => setShowModal(true)}>Book</CreateBookingButton>
+          <CreateBookingButton onClick={() => setShowModal(true)}>
+            Book
+          </CreateBookingButton>
         </div>
-    </div>
-        
-        {showModal && 
-            <BookingModalLayout event={event} onClose={() => setShowModal(false)} onSuccess={() => {setSuccessMessage(`Booking to ${event.eventName} is completed!`); 
-            getEvents();
-        }}/>
-        }
-    </>
-    )
-}
+      </div>
 
-export default EventCard
+      {showModal && (
+        <BookingModalLayout
+          event={event}
+          onClose={() => setShowModal(false)}
+          onSuccess={() => {
+            setSuccessMessage(`Booking to ${event.eventName} is completed!`);
+          }}
+        />
+      )}
+    </>
+  );
+};
+
+export default EventCard;
